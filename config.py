9f27a3c134d0900f48adc07590fcaba41d8f4e49
ERROR404 <32724548+hebtuerror404@users.noreply.github.com>
2022-12-20 18:42:13 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+capeConfig = {
+    'Author': '小蓝蓝',
+    'CapeName': '蓝蓝的测试图标',
+    'CapeVersion': 1.0,
+    'Cursors': {
+        'com.apple.coregraphics.Arrow': {
+            'FrameDuration': 0.1,
+            'HotSpot': (0.0, 0.0),
+            'Size': (32.0, 32.0),
+            'ANIPath': "./busy.ani"
+        }
+    },
+    'HiDPI': False
+}
